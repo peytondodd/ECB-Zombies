f134d50a2f25f2a14e7c81b0ce319d13f09a0557
William Johann <williamjohann44@gmail.com>
2013-02-24 21:29:38 -0600

    Removed 3 not needed imports

--- src/main/java/net/endercraftbuild/zombies/listeners/EntityListener.java
+++ src/main/java/net/endercraftbuild/zombies/listeners/EntityListener.java
@@ -7,7 +7,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.event.entity.EntityDeathEvent;
 
 public class EntityListener implements Listener
 {
--- src/main/java/net/endercraftbuild/zombies/listeners/PlayerListener.java
+++ src/main/java/net/endercraftbuild/zombies/listeners/PlayerListener.java
@@ -9,14 +9,12 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.entity.Snowball;
 
 public class PlayerListener implements Listener {
 
