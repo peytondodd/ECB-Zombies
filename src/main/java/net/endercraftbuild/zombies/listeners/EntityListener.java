665b290f47d45954d1d6bd5a2382539038f39c3e
Calin P <superpeanut911@gmail.com>
2013-02-25 19:26:37 -0500

    Added Hellhounds
    Added Hellhounds + no player explosive damage if in a zombies game

--- src/main/java/net/endercraftbuild/zombies/listeners/EntityListener.java
+++ src/main/java/net/endercraftbuild/zombies/listeners/EntityListener.java
@@ -1,10 +1,14 @@
 package net.endercraftbuild.zombies.listeners;
 
 import net.endercraftbuild.zombies.ZombiesMain;
+import net.endercraftbuild.zombies.utils.Utils;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
@@ -15,7 +19,7 @@ public class EntityListener implements Listener
 	public EntityListener(ZombiesMain plugin) {
 		this.plugin = plugin;
 	}
-	
+
 	@EventHandler
 	public void BurningWolf(EntityDamageEvent event)
 	{
@@ -25,6 +29,35 @@ public class EntityListener implements Listener
 			{
 				event.setCancelled(true);
 			}
+			//No explosion damage to players in ZOMBIE games only!
+			else if(event.getEntity() instanceof Player)
+			{ 
+				Player player = (Player) event.getEntity();
+
+				if(Utils.isInGameZ(player) == true)
+				{
+					if(event.getCause().equals(DamageCause.ENTITY_EXPLOSION) || event.getCause().equals(DamageCause.BLOCK_EXPLOSION))
+					{
+						event.setCancelled(true);
+					}
+				}
+			}
+		}
+	}
+
+	@EventHandler
+	public void hellhound(CreatureSpawnEvent event) {
+		if(event.isCancelled())
+			return;
+
+		Entity ent = event.getEntity();
+		if(ent instanceof Wolf) {
+			Wolf w = (Wolf)event.getEntity();
+			w.setFireTicks(1000000000); //Keep him on fire for a long time...
 		}
 	}
 }
+
+
+
+
