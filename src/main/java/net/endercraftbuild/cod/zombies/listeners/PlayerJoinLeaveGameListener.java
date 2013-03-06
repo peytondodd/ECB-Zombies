c984adbce7f3ce10e43c9df5e0b205c2a7731646
Calin P <superpeanut911@gmail.com>
2013-03-06 18:47:53 -0500

    Added kits
    Some base kit stuff

--- src/main/java/net/endercraftbuild/cod/zombies/listeners/PlayerJoinLeaveGameListener.java
+++ src/main/java/net/endercraftbuild/cod/zombies/listeners/PlayerJoinLeaveGameListener.java
@@ -1,14 +1,19 @@
 package net.endercraftbuild.cod.zombies.listeners;
 
+import java.util.List;
+
 import net.endercraftbuild.cod.CoDMain;
 import net.endercraftbuild.cod.events.PlayerJoinEvent;
 import net.endercraftbuild.cod.events.PlayerLeaveEvent;
+import net.endercraftbuild.cod.utils.Utils;
 import net.endercraftbuild.cod.zombies.ZombieGame;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class PlayerJoinLeaveGameListener implements Listener {
 
@@ -20,10 +25,35 @@ public class PlayerJoinLeaveGameListener implements Listener {
 	
 	@EventHandler(priority = EventPriority.LOW)
 	public void onPlayerJoin(PlayerJoinEvent event) {
-		ZombieGame game = (ZombieGame) event.getGame();
 		Player player = event.getPlayer();
+		ZombieGame game = (ZombieGame) event.getGame();
 		reset(player);
 		player.teleport(game.getSpawnLocation());
+		//kits
+		if(player.hasPermission("ecb.cod.donorkit")) 
+		{
+		List<Integer> dkit = plugin.getConfig().getIntegerList("kits.items.donor");
+		PlayerInventory inv = player.getInventory();
+		for (Integer id : dkit) {
+		ItemStack givedonorkit = new ItemStack(id, 1);
+		ItemStack ammo = new ItemStack(337, 64);//Just plain give ammo by default, too lazy to add multiple item config
+		inv.addItem(givedonorkit);
+		ammo = Utils.setItemName(ammo, "Bullets");//rename to bullets!
+		inv.addItem(ammo);
+			}
+		}
+		else 
+			{
+			List<Integer> kit = plugin.getConfig().getIntegerList("kits.items.normal");
+			PlayerInventory inv = player.getInventory();
+			for (Integer id : kit) {
+			ItemStack givekit = new ItemStack(id, 1);
+			ItemStack ammo = new ItemStack(337, 64);//Just plain give ammo by default, too lazy to add multiple item config
+			ammo = Utils.setItemName(ammo, "Bullets");
+			inv.addItem(ammo);
+			inv.addItem(givekit);
+			}
+		}
 	}
 	
 	@EventHandler(priority = EventPriority.HIGH)
